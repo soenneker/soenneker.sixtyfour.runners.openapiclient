3c498111daad731db3e93f4a5d0485bc1243966f
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:38 -0500

    TUnit conversion

--- test/Soenneker.Sixtyfour.Runners.OpenApiClient.Tests/SixtyfourOpenApiClientRunnerTests.cs
+++ test/Soenneker.Sixtyfour.Runners.OpenApiClient.Tests/SixtyfourOpenApiClientRunnerTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Sixtyfour.Runners.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class SixtyfourOpenApiClientRunnerTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class SixtyfourOpenApiClientRunnerTests : HostedUnitTest
 {
-    public SixtyfourOpenApiClientRunnerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public SixtyfourOpenApiClientRunnerTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
